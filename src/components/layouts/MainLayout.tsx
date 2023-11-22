a249c0bf8e659cc00f8c0eb39a9550d8e8f62004
Bohdan Kushnir <140017305+geniusFulldev@users.noreply.github.com>
2023-11-23 02:47:55 +0300

    Built the navbar
    * Built the navbar
    
    * Fixed deployment issue
    
    * Made all the header sections appear when screen size varies
    
    * Added links to the navbar and update logout button
    
    * add routes and active link class
    
    ---------
    
    Co-authored-by: maximiliano carrasco <maxxii.1820@gmail.com>

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MainHeader from "./MainHeader";
+
+const MainLayout = ({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}): JSX.Element => {
+  return (
+    <>
+      <MainHeader />
+      {children}
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/integration.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "@/components/layouts/MainLayout";
+import React, { ReactNode } from "react";
+
+const IntegrationsPage = (): ReactNode => {
+  return <div>IntegrationsPage</div>;
+};
+
+IntegrationsPage.Layout = MainLayout;
+
+export default IntegrationsPage;
