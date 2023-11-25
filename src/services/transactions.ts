98afae4b7f47d887a2b0dec4f8c9240bbf82f7b2
geniusFulldev <kushnir.genious@gmail.com>
2023-11-25 02:32:37 +0200

    Built the transaction page - issue17_1
    Added non-set warning to each row

--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -26,14 +26,43 @@ export interface TransactionProps {
     amount: number;
     price: number;
   };
+  currencyMapping?: {
+    currency: string | null,
+    token: string | null,
+    nonSetMapping: boolean    
+  }
+}
+
+export interface NonSetCurrencyProps {
+  currency: string,
+  token: string
 }
 
 export interface TransactionAPIResult {
   transactions: TransactionProps[];
+  non_set_currencies?: NonSetCurrencyProps[];
 }
 
 export const fetchTransactions = async (): Promise<TransactionAPIResult> => {
   const ret = await fetch("/data/transactions.json");
-  const data = await ret.json();
+  if( !ret.ok ) {
+    throw new Error(ret.statusText);
+  }
+  const data:TransactionAPIResult = await ret.json();
+  if( data.transactions ) {
+    for(const transaction of data.transactions) {
+      const currency = transaction.from.detail?.type === 'currency'? transaction.from.detail.symbol: (transaction.to.detail?.type === 'currency'? transaction.to.detail.symbol: null );
+      const token = transaction.to.detail?.type === 'token'? transaction.to.detail.symbol: (transaction.from.detail?.type === 'token'? transaction.from.detail.symbol: null );
+      let nonSetMapping = false;
+      if( data.non_set_currencies ) {
+        nonSetMapping = !!data.non_set_currencies.find(item => item.currency === currency && item.token === token );
+      }
+      transaction.currencyMapping = {
+        currency, 
+        token,
+        nonSetMapping
+      }
+    }
+  }
   return data;
 };
