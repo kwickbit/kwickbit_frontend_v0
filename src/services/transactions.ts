7eb84d66de2d34ebb4f3aa259c4e793922f62db1
geniusFulldev <kushnir.genious@gmail.com>
2023-11-25 02:32:37 +0200

    Built the transaction page - issue17_1
    Added non-set warning to each row
    
    Updated service transactions function

--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -7,13 +7,13 @@ export interface TransUser {
     amount: number;
     price: number;
     metadata?: {
-      [key:string]: string
-    }
+      [key: string]: string;
+    };
   };
 }
 
 export interface TransactionProps {
-  id?: number | string; 
+  id?: number | string;
   createdAt: string;
   from: TransUser;
   to: TransUser;
@@ -26,14 +26,69 @@ export interface TransactionProps {
     amount: number;
     price: number;
   };
+  currencyMapping?: {
+    currency: string | null;
+    token: string | null;
+    nonSetMapping: boolean;
+  };
+}
+
+export interface NonSetCurrencyProps {
+  currency: string;
+  token: string;
 }
 
 export interface TransactionAPIResult {
   transactions: TransactionProps[];
+  non_set_currencies?: NonSetCurrencyProps[];
 }
 
 export const fetchTransactions = async (): Promise<TransactionAPIResult> => {
   const ret = await fetch("/data/transactions.json");
-  const data = await ret.json();
+  if (!ret.ok) {
+    throw new Error(ret.statusText);
+  }
+  const data: TransactionAPIResult = await ret.json();
+  if (data.transactions) {
+    const transactions = data.transactions.map((transaction) => {
+      const fromDetail = transaction.from.detail;
+      const toDetail = transaction.to.detail;
+
+      let currency: string | null = null;
+      let token: string | null = null;
+
+      if (fromDetail?.type === "currency") {
+        currency = fromDetail.symbol;
+      } else if (toDetail?.type === "currency") {
+        currency = toDetail.symbol;
+      }
+
+      if (toDetail?.type === "token") {
+        token = toDetail.symbol;
+      } else if (fromDetail?.type === "token") {
+        token = fromDetail.symbol;
+      }
+
+      let nonSetMapping = false;
+      if (data.non_set_currencies) {
+        nonSetMapping = !!data.non_set_currencies.find(
+          (item) => item.currency === currency && item.token === token
+        );
+      }
+      return {
+        ...transaction,
+        currencyMapping: {
+          currency,
+          token,
+          nonSetMapping,
+        },
+      };
+    });
+
+    return {
+      ...data,
+      transactions,
+    };
+  }
   return data;
 };
