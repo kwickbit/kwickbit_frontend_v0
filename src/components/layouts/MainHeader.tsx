3dae578b9138da654e47fa83d00d8b39778c1dc4
maximiliano carrasco <56497105+abstract829@users.noreply.github.com>
2023-11-26 05:24:07 -0800

    Improve styles (#29)
    * Improve styles

--- src/components/layouts/MainHeader.tsx
+++ src/components/layouts/MainHeader.tsx
@@ -31,7 +31,7 @@ const MainHeader = (): JSX.Element | null => {
 
   return (
     <div className="z-50 my-14 container mx-auto max-w-7xl px-4">
-      <div className="border-2 rounded-[20px] border-[#BDC1CA] relative flex items-center justify-between py-3 px-6">
+      <div className="border-2 rounded-[20px] border-[#BDC1CA] bg-white relative flex items-center justify-between py-3 px-6">
         <div className="flex-1 flex items-center gap-5 xl:gap-7">
           <Link href="/" passHref className="flex-shrink-0 flex items-center">
             <Logo className="w-[158px] h-[24px]" />
