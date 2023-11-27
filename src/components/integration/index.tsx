83e76beae91b8e48d7fe6a427a38e9990bc1ae5a
MohsinGhani <mohsinghani.777@gmail.com>
2023-11-27 10:49:16 +0500

    Refactor integration component styles

--- src/components/integration/index.tsx
+++ src/components/integration/index.tsx
@@ -5,26 +5,22 @@ const Integration = () => {
   return (
     <div className="max-w-7xl mx-auto overflow-x-auto mt-12 px-4 pb-12">
       <h1>Integrations</h1>
-      <div className="mt-[30px]">
-        <div className="border rounded-[4px] px-[14px] pt-[8px] pb-[16px] w-fit">
-          <div className="flex items-center gap-[15px] py-[16px] pl-[26px] bg-[#D7F0FBFF]">
+      <div className="mt-8">
+        <div className="border rounded-md px-4 pt-2 pb-4 w-fit">
+          <div className="flex items-center gap-4 py-4 pl-4 bg-[#D7F0FBFF]">
             <div>
               <img src="/assets/quickbook-logo.png" alt="quickbooks" />
             </div>
             <div>
-              <p className="text-[16px] text-[#171A1FFF] font-bold">
-                Quickbooks
-              </p>
-              <p className="text-[16px] text-[#9095A1FF] font-bold">
-                Accounting
-              </p>
+              <p className="text-base text-[#171A1FFF] font-bold">Quickbooks</p>
+              <p className="text-base text-[#9095A1FF] font-bold">Accounting</p>
             </div>
           </div>
-          <div className="flex flex-col justify-between pt-[14px] px-[14px] h-[260px]">
-            <p className="text-[16px] text-[#9095A1FF] font-bold">
+          <div className="flex flex-col justify-between pt-4 px-4 h-64">
+            <p className="text-base text-[#9095A1FF] font-bold">
               Connecting a Quickbooks account
             </p>
-            <PrimaryButton className="bg-[#4ADDB6FF] text-[#21254EFF] w-[76px] py-[10px] px-[4px] rounded-[11px] text-[16px]">
+            <PrimaryButton className="bg-[#4ADDB6FF] text-[#21254EFF] w-20 py-3 px-1 rounded-xl text-base">
               Connect
             </PrimaryButton>
           </div>
