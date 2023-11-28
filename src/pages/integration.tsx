7f367f6a616fef7c36e09fb94767452b7694260c
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-11-28 07:43:24 -0800

    Add Integration component to IntegrationsPage
    * Add Integration component to IntegrationsPage
    
    * Add Quickbooks logo to integration component
    
    * Refactor integration component styles
    
    * Fixed missing return type on function
    
    * Update Quickbooks integration styling

--- src/pages/integration.tsx
+++ src/pages/integration.tsx
@@ -1,8 +1,9 @@
+import Integration from "@/components/integration";
 import MainLayout from "@/components/layouts/MainLayout";
 import React, { ReactNode } from "react";
 
 const IntegrationsPage = (): ReactNode => {
-  return <div>IntegrationsPage</div>;
+  return <Integration />;
 };
 
 IntegrationsPage.Layout = MainLayout;
