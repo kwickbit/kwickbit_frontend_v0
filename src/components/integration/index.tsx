e5b2fda3de0a2eeb068e13137c512705c69af30a
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-11-28 07:43:24 -0800

    Add Integration component to IntegrationsPage
    * Add Integration component to IntegrationsPage
    
    * Add Quickbooks logo to integration component
    
    * Refactor integration component styles
    
    * Fixed missing return type on function
    
    * Update Quickbooks integration styling

---
+++ src/components/integration/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PrimaryButton from "../PrimaryButton";
+
+const Integration: React.FC = (): JSX.Element => {
+  return (
+    <div className="max-w-7xl mx-auto overflow-x-auto mt-12 px-4 pb-12">
+      <h1>Integrations</h1>
+      <div className="mt-8">
+        <div className="border rounded-md px-4 pt-2 pb-4 w-fit">
+          <div className="flex items-center gap-4 py-4 pl-4 bg-[#D7F0FBFF]">
+            <div>
+              <img src="/assets/quickbook-logo.png" alt="quickbooks" />
+            </div>
+            <div>
+              <p className="text-base text-neutral-900 font-bold">Quickbooks</p>
+              <p className="text-base text-neutral-500 font-bold">Accounting</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between pt-4 px-4 h-64">
+            <p className="text-base text-neutral-500 font-bold">
+              Connecting a Quickbooks account
+            </p>
+            <PrimaryButton className="bg-[#4ADDB6FF] text-[#21254EFF] w-fit py-3 px-1 rounded-xl text-base">
+              Connect
+            </PrimaryButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Integration;
