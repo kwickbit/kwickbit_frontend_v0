4d1a3c0a2c1114878fcc767900824fd84fd8876d
maxi <maxi@MacBook-Air-de-maxi.local>
2023-11-28 19:19:55 -0300

    fix ws
    fix ws
    
    validate isConnected to connect ws
    
    fix ws
    
    fix ws by validating isConnected to connect ws

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import MainHeader from "./MainHeader";
+import { useUserWebSocket } from "@/hooks/useWebSocket";
 
 const MainLayout = ({
   children,
 }: {
   children: JSX.Element | JSX.Element[];
 }): JSX.Element => {
+  const sockets = useUserWebSocket();
+
   return (
     <>
       <MainHeader />
--- src/hooks/useWebSocket.ts
+++ src/hooks/useWebSocket.ts
@@ -1,48 +1,52 @@
-import useWebSocket, { SendMessage } from 'react-use-websocket';
-
+import { isConnected } from "@/lib/session";
+import useWebSocket, { SendMessage } from "react-use-websocket";
 
 export interface UserWebSocketReturn {
-    sendMessage: SendMessage;
-    lastMessage: MessageEvent<any> | null;
-    readyState: WebSocket['readyState'];
+  sendMessage: SendMessage;
+  lastMessage: MessageEvent<any> | null;
+  readyState: WebSocket["readyState"];
 }
 
-const onError = (error: WebSocketEventMap['error']): void => {
-    console.error('WebSocket error:', error);
+const onError = (error: WebSocketEventMap["error"]): void => {
+  console.error("WebSocket error:", error);
 };
 
-const onClose = (event: WebSocketEventMap['close']): void => {
-    console.log('WebSocket closed:', event);
+const onClose = (event: WebSocketEventMap["close"]): void => {
+  console.log("WebSocket closed:", event);
 };
 
-const onOpen = (event: WebSocketEventMap['open']): void => {
-    console.log('WebSocket open:', event);
+const onOpen = (event: WebSocketEventMap["open"]): void => {
+  console.log("WebSocket open:", event);
 };
 
-const onMessage = (event: WebSocketEventMap['message']): void => {
-    console.log('WebSocket message:', event.data);
+const onMessage = (event: WebSocketEventMap["message"]): void => {
+  console.log("WebSocket message:", event.data);
 };
 
 const onReconnectStop = (numAttempts: number): void => {
-    console.error(`Failed to reconnect. Tried ${numAttempts} times`);
+  console.error(`Failed to reconnect. Tried ${numAttempts} times`);
 };
 
-export const useUserWebSocket = (url: string, shouldConnect: boolean): UserWebSocketReturn => {
-    const {
-        sendMessage,
-        lastMessage,
-        readyState,
-        // other returned values and methods
-    } = useWebSocket(url, {
-        shouldReconnect: () => true,
-        onError,
-        onClose,
-        onOpen,
-        onMessage,
-        onReconnectStop,
-        reconnectInterval: 3000, // in ms
-        reconnectAttempts: 10,
-    }, shouldConnect);
+export const useUserWebSocket = (): UserWebSocketReturn => {
+  const {
+    sendMessage,
+    lastMessage,
+    readyState,
+    // other returned values and methods
+  } = useWebSocket(
+    process.env.NEXT_PUBLIC_WS_APP_URL!,
+    {
+      shouldReconnect: () => true,
+      onError,
+      onClose,
+      onOpen,
+      onMessage,
+      onReconnectStop,
+      reconnectInterval: 3000, // in ms
+      reconnectAttempts: 10,
+    },
+    isConnected()
+  );
 
-    return { sendMessage, lastMessage, readyState };
+  return { sendMessage, lastMessage, readyState };
 };
