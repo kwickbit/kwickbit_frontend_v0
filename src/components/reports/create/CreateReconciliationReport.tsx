356e7310277658b56670a73109d6299099a954ea
Bruno Parga <brunoparga@gmail.com>
2024-03-27 13:36:16 +0200

    Make a slightly fuller CreateReconciliationReport

--- src/components/reports/create/CreateReconciliationReport.tsx
+++ src/components/reports/create/CreateReconciliationReport.tsx
@@ -2,9 +2,32 @@ import { ReactNode } from "react";
 
 const CreateReconciliationReport = (): ReactNode => {
 
+  // Mock transactions; later, these will be mapped from state
+  const transactions = [
+    "paid 100 XLM to John for lunch",
+    "received 50 XLM from Alice for concert tickets",
+    "paid 20 XLM to Bob for coffee",
+  ]
+
+  const displayTransaction = (transaction: string, index: number): React.JSX.Element => (
+    // Later, when the transaction is complete, we'll have some better key here
+    <div key={index} className="mb-2">
+      <li
+        className="outline-none border border-gray-500 py-3 px-3 w-full rounded-md col-span-5"
+      >
+        {transaction}
+      </li>
+    </div>
+  )
+
   return (
     <>
-      <span>TODO</span>
+      <span className="text-base text-[#21254E] mx-12">You have {transactions.length} unreconciled transactions.</span>
+      <div className="col-span-1 flex items-center gap-8 mx-12 my-6">
+        <ul>
+          {transactions.map(displayTransaction)}
+        </ul>
+      </div>
     </>
   )
 }
