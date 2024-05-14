5d999bc79e0d3d8d542c3d05099e6550e27b7694
Bruno Parga <brunoparga@gmail.com>
2024-05-14 12:20:59 +0100

    Show the user their new API key

--- src/components/api_keys/create/NewAPIKeyModal.tsx
+++ src/components/api_keys/create/NewAPIKeyModal.tsx
@@ -1,6 +1,7 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import classNames from "classnames";
 import Modal from "@/components/Modal";
 import { UseBooleanReturnProps } from "@/hooks/useBoolean";
-import { Dispatch, SetStateAction } from "react";
 
 interface Props {
   shouldShowNewAPIKey: UseBooleanReturnProps;
@@ -13,6 +14,25 @@ export const NewAPIKeyModal = ({
   newAPIKey,
   setNewAPIKey,
 }: Props): React.JSX.Element => {
+  const [keyIsCopied, setKeyIsCopied] = useState(false);
+  const [keyIsShown, setKeyIsShown] = useState(false);
+
+  const toggleShowKey = (): void => {
+    setKeyIsShown(!keyIsShown);
+  };
+
+  const maskedKey = newAPIKey.replaceAll(/./g, "*")
+
+  const copyKeyToClipboard = async (): Promise<void> => {
+    try {
+      await navigator.clipboard.writeText(newAPIKey);
+      setKeyIsCopied(true);
+      setTimeout(() => setKeyIsCopied(false), 2000);
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
   const closeModal = (): void => {
     shouldShowNewAPIKey.onFalse();
     setNewAPIKey("");
@@ -25,7 +45,35 @@ export const NewAPIKeyModal = ({
       show={shouldShowNewAPIKey.value}
       closeModal={closeModal}
     >
-      <p>Your new API key is {newAPIKey}.</p>
+      <div className="flex justify-between mb-4">
+        <div>
+          <p>This is your new API key. You need to put it somewhere safe.</p>
+          <p className="font-bold">The key will not be shown to you again.</p>
+          <p>Store it safely now.</p>
+        </div>
+        <button
+          className={classNames(
+            "rounded-md font-bold text-lg text-white px-6 py-2 min-w-[160px]",
+            keyIsCopied ? "bg-green-400" : "bg-sky-400"
+          )}
+          onClick={copyKeyToClipboard}
+        >
+          {keyIsCopied ? "Copied!" : "Copy key"}
+        </button>
+      </div>
+      <div className="flex flex-wrap bg-[#F3F4F6]">
+        <div
+          className="flex-1 flex items-center justify-center font-bold text-lg underline"
+          onClick={toggleShowKey}
+        >
+          {keyIsShown ? "Hide key" : "Show key"}
+        </div>
+        <p
+          className="break-words p-2 flex-shrink max-w-full"
+        >
+          {keyIsShown ? newAPIKey : maskedKey}
+        </p>
+      </div>
     </Modal>
   );
 };
