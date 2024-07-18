afe0a1e34a53cecb7d9f27eddf5a7cd10f51b962
Bruno Parga <brunoparga@gmail.com>
2024-07-18 16:27:41 +0300

    Prevent a ton of useless backend and API calls

--- src/hooks/useTokenMappingsDataProvider.tsx
+++ src/hooks/useTokenMappingsDataProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import {QueryObserverResult, RefetchOptions, useQuery, UseQueryResult} from "@tanstack/react-query";
 import { CurrencyMapping, CurrencyMappingAPIResult, getCurrencyMappings } from "@/services/integrations/quickbooks";
 
@@ -13,16 +13,19 @@ interface TokenMappingData {
         isError: boolean;
         refetch: UseQueryResult<CurrencyMappingAPIResult>['refetch'];
     };
+    enableCurrencyMappingsFetching: () => void;
 }
 
 const noOpAsync = (): Promise<void> => new Promise<void>((resolve) => resolve());
-const defaultContextValue = {
+
+const defaultContextValue: TokenMappingData = {
     currencyMappings: {
         data: [],
         isLoading: false,
         isError: false,
         refetch: noOpAsync as unknown as ((options?: RefetchOptions | undefined) => Promise<QueryObserverResult<CurrencyMappingAPIResult>>),
     },
+    enableCurrencyMappingsFetching: () => {},
 };
 
 const TokenMappingContext = createContext<TokenMappingData>(defaultContextValue);
@@ -30,10 +33,29 @@ const TokenMappingContext = createContext<TokenMappingData>(defaultContextValue)
 export const useTokenMappingContext = (): TokenMappingData => useContext(TokenMappingContext);
 
 export const TokenMappingDataProvider = ({ children }: Props): React.JSX.Element => {
-    const { data: currencyMappingsAPIResult, isLoading: isLoadingCurrencyMappings, isError: isErrorCurrencyMappings, refetch: refetchCurrencyMappings } = useQuery({queryKey: ['getCurrencyMappings'], queryFn: getCurrencyMappings});
+    const [shouldFetchCurrencyMappings, setShouldFetchCurrencyMappings] = useState(false);
+
+    const {
+        data: currencyMappingsAPIResult,
+        isLoading: isLoadingCurrencyMappings,
+        isError: isErrorCurrencyMappings,
+        refetch: refetchCurrencyMappings
+    } = useQuery(
+        {
+            queryKey: ['getCurrencyMappings'],
+            queryFn: getCurrencyMappings,
+            enabled: shouldFetchCurrencyMappings,
+        }
+    );
 
     const value = {
-        currencyMappings: { data: currencyMappingsAPIResult?.data || [], isLoading: isLoadingCurrencyMappings, isError: isErrorCurrencyMappings, refetch: refetchCurrencyMappings },
+        currencyMappings: {
+            data: currencyMappingsAPIResult?.data || [],
+            isLoading: isLoadingCurrencyMappings,
+            isError: isErrorCurrencyMappings,
+            refetch: refetchCurrencyMappings
+        },
+        enableCurrencyMappingsFetching: () => setShouldFetchCurrencyMappings(true)
     };
 
     return (
