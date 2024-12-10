0b6e37e155b5ce457f5de1c96f8ad95116659fc9
Bruno Parga <brunoparga@gmail.com>
2024-12-10 16:53:57 +0200

    Show error message on build gains report failure

--- src/components/reports/GainsReportsView.tsx
+++ src/components/reports/GainsReportsView.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 import { useBoolean } from "@/hooks/useBoolean";
 import useUserWebSocket from "@/hooks/useWebSocket";
 import { useQueryAllGainsReports } from "@/hooks/reports/gains";
@@ -11,7 +13,13 @@ import { GainsReportList } from "@/components/reports/gains/GainsReportList";
 export const GainsReportsView = (): JSX.Element => {
   const { data, isLoading, isError } = useQueryAllGainsReports();
   const showModal = useBoolean();
-  const { newGainsReport: newReport } = useUserWebSocket();
+
+  const {
+    newGainsReport: newReport,
+    newGainsReportError: newReportError,
+    setNewGainsReportError: setNewReportError
+  } = useUserWebSocket();
+
   const [reports, setReports] = useState(data?.data ?? []);
   const [newReportId, setNewReportId] = useState<string | undefined>(undefined);
 
@@ -26,6 +34,13 @@ export const GainsReportsView = (): JSX.Element => {
       setTimeout(() => setNewReportId(undefined), 10_000);
     }
   }, [newReport]);
+
+  useEffect(() => {
+    if (newReportError) {
+      toast.error(`Error building gains report: ${newReportError}`);
+      setNewReportError(null);
+    }
+  }, [newReportError, setNewReportError]);
 
   if (isLoading) {
     return (
